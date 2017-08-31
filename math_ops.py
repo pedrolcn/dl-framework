9652602aa8e3fbaced4eb89e4d456e034373e114
Pedro Nascimento <pedrolcn@gmail.com>
2017-08-31 21:27:55 +0200

    Feat: added BinaryOp wrapper class, together with the first mathOp, added methods in Graph and Node classes to interact with MathOps

---
+++ math_ops.py
@@ -0,0 +1,22 @@
+# import numpy as np
+from graph import Node
+
+
+class BinaryOp(Node):
+    """
+    Abstract class for general binary operations
+    """
+    def __init__(self, a, b, bin_op, graph=None, name=None):
+        if a not in graph.nodes.values():
+            raise NameError('Node %s not in the specified graph'.format(a))
+        elif b not in graph.nodes.values():
+            raise NameError('Node %s not in the specified graph'.format(b))
+        else:
+            parents = graph.get_parents_by_var((a, b))
+
+        super().__init__(graph=graph, op=bin_op, parents=parents, cache=True, name=name)
+
+    def forward(self):
+
+        input_values = self.get_cached_outputs_from_parents()
+        self.cached_output = self.op(*input_values)
